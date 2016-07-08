3c4f3dd7732ec7b80da883b5bcc4829f7f2b874c
SuvethaR <suvetha.rkn@gmail.com>
2016-07-08 16:00:15 +0530

    Update and rename README.md to suve.c

---
+++ suve.c
@@ -0,0 +1,17 @@
+# pos
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter a number");
+scanf("%d",a);
+if(a>0&&a<0)
+{
+if(a>=0)
+printf("number is positive");
+else
+printf("number is negative");
+else
+printf("number is zero");
+}
+}
